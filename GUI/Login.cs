aa548abd0885cf88ca741e720b1a4e5d5f1e9c3a
Nico Ohms <Nico.Ohms@schueler.ers-hm.de>
2020-12-01 12:37:38 +0100

    AD Anmeldung

--- GUI/Login.cs
+++ GUI/Login.cs
@@ -9,7 +9,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WarenhausManagement.GUI;
+using System.DirectoryServices;
 using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.Protocols;
+using System.Security;
 
 namespace WarenhausManagement
 {
@@ -29,13 +32,25 @@ namespace WarenhausManagement
             if (user.GetUsername()!= "" && user.GetPassword() != null)
             {
                 //Login mit DB oder AD
+                string AnmeldeName = txtbx_Username.Text;
+                string AnmeldePw = txtbx_Password.Text;
+                
+                bool AnmeldungGültig = LDAPConnection(AnmeldeName, AnmeldePw);
+                if (AnmeldungGültig == true)
+                {
+                    Mainmenu hmenu = new Mainmenu(user);
+                    this.Hide();
+                    hmenu.Show();
+                }
+                else
+                {
+                    lbl_Status.Text = "Eingaben nicht vollständig";
+                }
                 //nächste Form aufrufen nur wenn erfolgreich
                 //user für Testzwecke
-                user.SetUsername("SA");
-                user.SetPassword("Ers1234Ers1234");
-                Mainmenu hmenu = new Mainmenu(user);
-                this.Hide();
-                hmenu.Show();
+                //user.SetUsername("SA");
+                //user.SetPassword("Ers1234Ers1234");
+
             }
             else
             {
@@ -57,6 +72,63 @@ namespace WarenhausManagement
                 sb.Append(hashBytes[i].ToString("X2"));
             }
             return sb.ToString();
+        }
+        public bool LDAPConnection(string username, string password)
+        {
+            SecureString pwd = new SecureString();
+            bool bAuth = false;
+            DirectoryEntry entry = null;
+
+
+
+            // erstellt und kehrt zurück zu der LDAP Verbindung
+
+
+
+            DirectoryEntry ldapConnection = new DirectoryEntry("WHM.local");
+            ldapConnection.Path = "LDAP://CN=DBUser,DC=WHM,DC=local";
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+
+
+
+            foreach (char c in password)
+            {
+                pwd.AppendChar(c);
+            }
+
+
+
+            //Bewirkt, dass das Passwort nicht mehr verändert werden kann
+
+
+
+            pwd.MakeReadOnly();
+
+
+
+            //Passwort wird einem Pointer übergeben, damit dieser später "entschlüsselt" werden kann
+            IntPtr pPwd = System.Runtime.InteropServices.Marshal.SecureStringToBSTR(pwd);
+
+
+
+
+            try
+            {
+                entry = new DirectoryEntry(string.Concat(@"LDAP://WHM.local"), username, System.Runtime.InteropServices.Marshal.PtrToStringBSTR(pPwd));
+                object nativeObject = entry.NativeObject;
+                bAuth = true;
+            }
+            catch (Exception)
+            {
+                bAuth = false;
+            }
+            finally
+            {
+                entry.Close();
+                entry.Dispose();
+            }
+
+            return bAuth;
         }
 
         private void pictureBoxPW_Click(object sender, EventArgs e)
