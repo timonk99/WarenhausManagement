f0a83c1039858775d41199b9a7a61e1acd03ac46
AndreDMarquesCapela <fourseven.ac@gmail.com>
2021-02-05 12:14:54 +0100

    .

---
+++ Message.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WarenhausManagement
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_ok = new System.Windows.Forms.Button();
+            this.l_message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b_ok
+            // 
+            this.b_ok.Location = new System.Drawing.Point(103, 81);
+            this.b_ok.Name = "b_ok";
+            this.b_ok.Size = new System.Drawing.Size(75, 23);
+            this.b_ok.TabIndex = 0;
+            this.b_ok.Text = "Ok";
+            this.b_ok.UseVisualStyleBackColor = true;
+            this.b_ok.Click += new System.EventHandler(this.b_ok_Click);
+            // 
+            // l_message
+            // 
+            this.l_message.AutoSize = true;
+            this.l_message.Location = new System.Drawing.Point(45, 40);
+            this.l_message.Name = "l_message";
+            this.l_message.Size = new System.Drawing.Size(35, 13);
+            this.l_message.TabIndex = 1;
+            this.l_message.Text = "label1";
+            // 
+            // Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 116);
+            this.Controls.Add(this.l_message);
+            this.Controls.Add(this.b_ok);
+            this.Name = "Message";
+            this.Text = "Message";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_ok;
+        private System.Windows.Forms.Label l_message;
+    }
+}
